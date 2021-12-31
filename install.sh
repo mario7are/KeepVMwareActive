823916d146df589c1f6d899afd14e7b6fac0f8cf
mario7are <96884544+mario7are@users.noreply.github.com>
2021-12-30 22:56:49 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ echo "*******************************************"
 pip3 install -r ./requirements.txt
 script_full_path="$(pwd)/main.py"
 crontab -r
-(crontab -l 2>/dev/null; echo "*/9 * * * * python3 ${script_full_path}") | crontab -
+(crontab -l 2>/dev/null; echo "*/7 * * * * python3 ${script_full_path}") | crontab -
 crontab -l
 echo "Instalation completed!"
